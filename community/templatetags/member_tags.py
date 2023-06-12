b888a6db9d73e907caf201ab47374287547de74f
Tee-ombima <80878881+Tee-ombima@users.noreply.github.com>
2023-06-12 17:44:29 +0300

    emails

---
+++ community/templatetags/member_tags.py
@@ -0,0 +1,11 @@
+# member_tags.py
+from django import template
+from accounts.models import User
+
+register = template.Library()
+
+@register.simple_tag
+def get_member_emails():
+    users = User.objects.all()
+    emails = [user.email for user in users]
+    return emails
--- community/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from accounts.models import User
-
-def community_page(request):
-    users = User.objects.all()
-    return render(request, 'community_page.html', {'users': users})
