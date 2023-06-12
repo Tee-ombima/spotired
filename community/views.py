258b991055c6a5b367f36ef1215c430a4cabd0f1
Tee-ombima <80878881+Tee-ombima@users.noreply.github.com>
2023-06-12 15:52:52 +0300

    members

--- community/views.py
+++ community/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from accounts.models import User
 
 def community_page(request):
-    registered_users = User.objects.all()
-    return render(request, 'community_page.html', {'registered_users': registered_users})
+    users = User.objects.all()
+    return render(request, 'community_page.html', {'users': users})
