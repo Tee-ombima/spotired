c8b28d967085573ed514706aa50bf49c8bb3f8b8
Tee-ombima <80878881+Tee-ombima@users.noreply.github.com>
2023-06-13 15:46:57 +0300

    paginator

---
+++ community/views.py
@@ -0,0 +1,10 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from accounts.models import User
+
+def member_list(request):
+    users = User.objects.all()
+    paginator = Paginator(users, 20)  # Display 20 members per page
+    page_number = request.GET.get('page')
+    members = paginator.get_page(page_number)
+    return render(request, 'member_list.html', {'members': members})
