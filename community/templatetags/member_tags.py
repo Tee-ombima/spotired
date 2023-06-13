4b5787cf49e5726d34b2359c28d3ab38dc6ec4b4
Tee-ombima <80878881+Tee-ombima@users.noreply.github.com>
2023-06-13 17:03:37 +0300

    Update member_tags.py

--- community/templatetags/member_tags.py
+++ community/templatetags/member_tags.py
@@ -1,4 +1,3 @@
-# member_tags.py
 from django import template
 from accounts.models import User
 
@@ -7,6 +6,19 @@ register = template.Library()
 @register.simple_tag
 def get_member_emails():
     users = User.objects.all()
-    emails = [user.email for user in users]
-    return emails
+    member_info = [(user.email, user.username) for user in users]
+    return member_info
+
+
+# # member_tags.py
+# from django import template
+# from accounts.models import User
+
+# register = template.Library()
+
+# @register.simple_tag
+# def get_member_emails():
+#     users = User.objects.all()
+#     emails = [user.email for user in users]
+#     return emails
 
