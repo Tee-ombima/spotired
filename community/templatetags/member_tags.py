0dc61f8ce52b9b6e9d1ff143e0345847b4a8786b
Tee-ombima <80878881+Tee-ombima@users.noreply.github.com>
2023-06-13 14:17:07 +0300

    members

--- community/templatetags/member_tags.py
+++ community/templatetags/member_tags.py
@@ -10,12 +10,14 @@
 #     emails = [user.email for user in users]
 #     return emails
 # member_tags.py
+# member_tags.py
 from django import template
 from accounts.models import User
 
 register = template.Library()
 
 @register.simple_tag
-def get_members():
-    members = User.objects.all()
-    return members
+def get_member_emails():
+    users = User.objects.all()
+    emails = [user.email for user in users]
+    return emails
